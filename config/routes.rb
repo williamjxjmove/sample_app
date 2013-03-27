9e6e2ba3a77e607e759dee6d99de372264273162
william.jiang@move.com <william.jiang@move.com>
2013-03-27 12:42:04 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
-  match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
 
