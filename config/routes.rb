edbcfbb0f4d135412d235f943b21a915c56e09b5
william.jiang@move.com <william.jiang@move.com>
2013-03-26 17:35:46 -0700

    root_path final work, not sure why???

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  get "users/new"
+
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #get "static_pages/about"
+  #get "static_pages/contact"
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
